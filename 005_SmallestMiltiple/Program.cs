007f74667b35a939b96bdda09b822772512bda22
ROBDAN99 <dan.buxton99@gmail.com>
2018-10-11 22:32:08 +0100

    Added slight efficiency to 005.

--- 005_SmallestMiltiple/Program.cs
+++ 005_SmallestMiltiple/Program.cs
@@ -10,12 +10,12 @@ namespace _005_SmallestMiltiple
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"The largest common multiple of 1-20: { SmallestMultiple() }");
+            Console.WriteLine($"The smallest common multiple of 1-20: { SmallestMultiple(maxDivisible: 20) }");
         }
 
-        public static int SmallestMultiple()
+        public static int SmallestMultiple(int maxDivisible)
         {
-            int i = 1;
+            int i = maxDivisible;
 
             while (i % 2 != 0 || i % 3 != 0 || i % 4 != 0 || i % 5 != 0 ||
                 i % 6 != 0 || i % 7 != 0 || i % 8 != 0 || i % 9 != 0 ||
@@ -23,7 +23,7 @@ namespace _005_SmallestMiltiple
                 i % 14 != 0 || i % 15 != 0 || i % 16 != 0 || i % 17 != 0 ||
                 i % 18 != 0 || i % 19 != 0 || i % 20 != 0)
             {
-                i += 20;
+                i += maxDivisible;
             }
 
             return i;
